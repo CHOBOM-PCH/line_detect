f3b7f2df0e02475b03a9a7ed29ae04cd0198d167
Chang-hee Park <iwojpc0pc@gmail.com>
2017-03-22 12:45:26 +0900

    12:45

--- Line_detection/line_detection.cpp
+++ Line_detection/line_detection.cpp
@@ -7,6 +7,7 @@ void Line_Detect(InputArray _src, OutputArray _dst, int* distance, double* radia
 	Mat gray_img;
 	Mat blur_img;
 	Mat thresoutput_image;
+	Mat edge_img;
 	vector<Vec4i>lines;
 	int length = 0;
 		
@@ -18,13 +19,13 @@ void Line_Detect(InputArray _src, OutputArray _dst, int* distance, double* radia
 	cvtColor(input_image,gray_img,CV_BGR2GRAY);
 	GaussianBlur(gray_img,blur_img,cv::Size(5,5),10);
 	adaptiveThreshold(blur_img, thresoutput_image, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 2);
-	imshow("image",thresoutput_image);
+	//imshow("image",thresoutput_image);
 	//line detect
-	HoughLinesP(thresoutput_image, lines, 1, PI/45, 10, 200, 0);
+	HoughLinesP(thresoutput_image, lines, 1, (PI / 45), 10, 200, 0);
 	//save line
 	Vec4d params,eparams;
 	int x1, y1, x2, y2;
-	int xe1=0, ye1=0, xe2=0, ye2 =0;
+	int xe1 = 0, ye1 = 0, xe2 = 0, ye2 = 0;
 	int e1=0,e2=0;
 	for (int k = 0; k < lines.size(); k++){
 		params = lines[k];
@@ -41,9 +42,9 @@ void Line_Detect(InputArray _src, OutputArray _dst, int* distance, double* radia
 		ye1 = (eparams[1]) / (k + 1);
 		xe2 = (eparams[2]) / (k + 1);
 		ye2 = (eparams[3]) / (k + 1);
-		Point pt1(x1,y1),pt2(x2,y2);
-		line(output_image,pt1,pt2, Scalar(255,0,255),1);
-		printf("선들의 좌표값 시작 x:%d y:%d 끝 x:%d y:%d \n",x1,y1,x2,y2);
+		Point pt1(x1, y1),pt2(x2, y2);
+		line(output_image, pt1, pt2, Scalar(255, 0, 255), 1);
+		printf("선들의 좌표값 시작 x:%d y:%d 끝 x:%d y:%d \n",x1, y1, x2, y2);
 	}
 	//print line and distance
 	if (xe1 == 0){
@@ -52,15 +53,15 @@ void Line_Detect(InputArray _src, OutputArray _dst, int* distance, double* radia
 	else {
 		e1 = xe1, e2 = xe2;
 		Point ept1(xe1,ye1),ept2(xe2,ye2);
-		line(output_image,ept1,ept2, Scalar(255,255,0),2);//평균선
+		line(output_image, ept1, ept2, Scalar(255, 255, 0), 2);//평균선
 
-		Point ct1(((input_image.cols)/2),0),ct2(((input_image.cols)/2),768);
-		line(output_image,ct1,ct2, Scalar(0,255,255),2);//중심선
+		Point ct1((input_image.cols / 2), 0),ct2((input_image.cols / 2), input_image.rows);
+		line(output_image, ct1, ct2, Scalar(0, 255, 255), 2);//중심선
 
-		length = ((input_image.cols)/2) - (xe1+xe2)/2 ;
-		*radian = (atan2f(((float)(ye1-ye2)),abs((float)(xe2-xe1))) *180/PI);
+		length = (input_image.cols / 2) - (xe1 + xe2) / 2 ;
+		*radian = (atan2f(((float)(ye1 - ye2)), abs((float)(xe2 - xe1))) * 180/PI);
 		*distance = length;
-		Point dp1((xe1+xe2)/2,300),dp2(((input_image.cols)/2),300);
+		Point dp1((xe1 + xe2) / 2, 300),dp2(((input_image.cols) / 2), 300);
 		//line(output_image,dp1,dp2, Scalar(255,0,255),1);
 		string te;
 		std::stringstream ste;
