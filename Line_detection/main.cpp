8f81ae84f7071e5ff08629333c8b00c3ff0ede12
Chang-hee Park <iwojpc0pc@gmail.com>
2017-04-04 16:48:27 +0900

    170404_16:48

--- Line_detection/main.cpp
+++ Line_detection/main.cpp
@@ -4,22 +4,15 @@
 
 int main()
 {
-	cv::Mat image = cv::imread("image/3.png", -1);
-	cv::Mat O_img;
+	
+	const char* k = "image/7.png";
 	int dist = 0;
 	double degree = 0;
+	Line_Detect(k, &dist, &degree);
 	
-	Line_Detect(image, O_img, &dist, &degree);
-	
-	
-	if (!O_img.empty()){
-	
-		cv::imshow("출력",O_img);
-
-	}
 	
 	printf("거리 %d \n", dist);
 	printf("기울어진 각도 %lf \n", degree);
 	cv::waitKey(0);
-	//system("pause");
+	
 }
